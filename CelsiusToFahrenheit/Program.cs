b35af337805b9e8f484a1c529239e112d076e530
AmirHosseinNarimaniRad <Amir.hossein.narimani.rad@gmail.com>
2025-05-10 18:06:32 +0330

    First Push

---
+++ CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CelsiusToFahrenheit
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number in Celsius :");
+            double TempC = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("in Farenheit : ");
+
+
+            double TempF = CelsiusToFahrenheit(TempC);
+            Console.WriteLine("In Fahrenheit : " + TempF);
+
+            Console.ReadLine();
+        }
+
+        //Method for Calculating the Fahrenheit Value.
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9 / 5) + 32;
+        }
+        
+    }
+}
